6032b614215d903233f171845c333612221f68dd
sanjib <sanjib.itm@gmail.com>
2025-01-22 14:07:28 +0530

    Printing with Python: Your First Step into Coding

---
+++ Python_EX1.py
@@ -0,0 +1,30 @@
+# Task 1: Print "Hello, World!"
+print("Hello, World!")
+
+# Task 2: Print multiple lines
+print("I'm learning Python!")
+print("This is so much fun!")
+print("I can't wait to learn more!")
+# Task 3: Use single and double quotes
+print('This is a string with single quotes.')
+print("This is a string with double quotes.")
+
+# Task 4: Print strings with quotes inside
+print("He said, 'Python is awesome!'")
+print('She replied, "I totally agree!"')
+
+# Task 5: Use escape characters
+print("First line\nSecond line")
+print("This\tis\ttabbed")
+
+# Task 6: Combine different data types
+print("My name is Python and I am", 30,"years old.")
+
+# Task 7: Get creative!
+print("Hi Team,")
+print("i am learning python today.")
+print("today i have started with print function.")
+print("which is helping to print the satement or string.")
+print('python is a popular language"now a days most of people using pthon is a programming language"')
+print("About Python:\n\t1.Python is a programming language.\n\t2.It's very is to learn and understand\n\t3.people are so excited to learn pthon now a days")
+print("Python is capture the market around percentage of ",60)
